aad3d4b0d0a0a248bb751107a31eddd66fde89e3
nine4-team <team@nine4.co>
2026-02-03 12:08:59 -1000

    fixed sync bug

---
+++ tmp/missing_ids_batch_5.sql
@@ -0,0 +1,8 @@
+with local as (select unnest(array['ecc33a2b-6c32-422a-b358-ca022c8fc986', 'ed1eba4b-4d64-4c9d-980e-faf296ff3f36', 'f0b0cb16-bb82-4e2a-abcb-7a3d02afea54', 'f9c0e07e-5385-447c-8cf9-dcd4a0f49105']::text[]) as item_id)
+select local.item_id as "itemId"
+from local
+left join public.items i
+  on i.account_id = '1dd4fd75-8eea-4f7a-98e7-bf45b987ae94'::uuid
+  and i.item_id = local.item_id
+where i.item_id is null
+order by local.item_id;
